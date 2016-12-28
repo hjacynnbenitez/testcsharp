e5f43713092611d683b02d4eb7f2c09f90446511
hjacynnbenitez <hjacynn.benitez@gmail.com>
2016-12-28 15:04:24 +0800

    added a button

--- MyAddressBook/MyAddressBook/Form1.cs
+++ MyAddressBook/MyAddressBook/Form1.cs
@@ -16,5 +16,10 @@ namespace MyAddressBook
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
     }
 }
